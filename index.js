9ec7f91e2a0169043b073cf8bb13c2cf0edd83fc
Patrick Jasinski <patrick.jasinski6800@me.com>
2021-10-27 23:53:58 -0400

    changed countdown timer to not use date and display counter

--- index.js
+++ index.js
@@ -16,27 +16,22 @@ function timerPrompt() {
         video.play();
     })
 }
+var photoTimer = setInterval(function() {
+  if (counter > 9) {
+    clearInterval(photoTimer);
+  } else {
+    $("#message").text(counter);
+  } 
+  counter++;
+}, 1000)
+/*function countdownTimer() {
 
-function countdownTimer() {
-  difference = +new Date(endTime) - +new Date();
   let remaining = "";
 
-  let now = new Date().getTime();
-  
-  if (difference > 0 && counter < 5) {
-    const parts = {
-      days: Math.floor(difference / (1000 * 60 * 60 * 24)),
-      hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
-      minutes: Math.floor((difference / 1000 / 60) % 60),
-      seconds: Math.floor((difference / 1000) % 60)
-    };
-    remaining =
-      // (parts.hours + "").padStart(2, "0") +
-      // ":" +
-      (parts.minutes + "").padStart(2, "0") +
-      ":" +
-      (parts.seconds + "").padStart(2, "0");
-      counter++;
+  $("#message").text("remaining");
+  if (counter < 5) {    
+    remaining = counter;
+    counter++;    
   }
   else if (counter > 5) {
     
@@ -48,24 +43,25 @@ function countdownTimer() {
       data: {img: imgData},    
     });
   }
+    
   document.getElementById("countdown").innerHTML = remaining;  
   console.log("upping counter to " + counter)
-  counter++; 
-}
+  counter++;
+  
+}*/
 
   var vid = document.getElementById("myVideo");
-  timer = timerPrompt();
+  
   var endTime = new Date();
-  endTime.setTime(new Date().getTime() + timer);
   console.log("initial run of countdownTimer()");
-  countdownTimer();
+  //countdownTimer();
   console.log("second run of countdownTimer()");
   $(document).keypress(function (e) { 
     if (e.which == 13 && counter > 5) {
       counter = 0;
     }
   });
-  setInterval(countdownTimer, 1000)
+  //setInterval(countdownTimer, 1000)
   /*
   $('#shutter').addClass('on');
   setTimeout(function() {
