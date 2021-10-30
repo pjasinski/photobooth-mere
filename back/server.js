9fd3b90deefd0656dd9027dc5cd0f94d511703fd
Patrick Jasinski <patrick.jasinski6800@me.com>
2021-10-30 13:39:32 -0400

    we'll do it live fuck it

--- back/server.js
+++ back/server.js
@@ -18,12 +18,18 @@ app.use(express.urlencoded({
   extended: true,
   limit: '50mb'
 }));
-const corsOptions = {
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+/*const corsOptions = {
   origin:'https://photobooth.patrickjasinski.com/',
   credentials: true,
   optionSuccessStatus:200
 }
 app.use(cors(corsOptions));
+*/
 const upload = multer({storage: storage});
 express.urlencoded();
 express.json()
