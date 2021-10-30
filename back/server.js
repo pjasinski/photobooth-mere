aa36b6766e938900bbe82e272083fa58e320165e
Patrick Jasinski <patrick.jasinski6800@me.com>
2021-10-30 10:23:57 -0400

    okay changing it back sorry

--- back/server.js
+++ back/server.js
@@ -35,6 +35,6 @@ app.post('/', upload.single('image'),(req, res, next) => {
 });
   imgCounter++;
 })
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 5000
 app.listen(port, 
     ()=>console.log(`Server running on port ${port}`))
